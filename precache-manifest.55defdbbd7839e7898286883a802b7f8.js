277b6ff7942365ea104be773af2b84e7b875e332
ortaib <ortea91@gmail.com>
2019-07-31 22:11:45 +0300

    Updates

---
+++ precache-manifest.55defdbbd7839e7898286883a802b7f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb50ed2115bd70eba967b82dbe20e91",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "8ca64c41dbfc1d1eb914",
+    "url": "/monster-rolodex/static/css/main.7541b247.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8ca64c41dbfc1d1eb914",
+    "url": "/monster-rolodex/static/js/main.27054d6e.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
