910a799440af3d1cb3f2f1aa41371d591be2cfee
ortaib <ortea91@gmail.com>
2019-07-31 22:02:07 +0300

    fixes

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -5,7 +5,7 @@ import { Card } from "../card/card.component";
 export const CardList = props => (
   <div className="card-list">
     {props.monsters.map(monster => (
-      <Card monster={monster} />
+      <Card key={monster.id} monster={monster} />
     ))}
   </div>
 );
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./card.styles.css";
+
 export const Card = props => (
-  <div key={props.monster.id} className="card-container">
+  <div className="card-container">
     <img
       alt="monster"
       src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
